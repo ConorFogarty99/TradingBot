7bea4f360a068012f2f61284b556e05074b8425b
ConorFogarty <46893180+ConorFogarty@users.noreply.github.com>
2021-05-05 21:32:32 +0100

    Getting api req

--- binance_futures.py
+++ binance_futures.py
@@ -1,3 +1,18 @@
 import logging
+import requests
+import pprint
 
 logger = logging.getLogger()
+
+
+def get_contracts():
+    res_obj = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+
+    contracts = []
+
+    for contract in res_obj.json()['symbols']:
+        contracts.append(contract['pair'])
+
+    return contracts
+
+print(get_contracts())
