a489bb2a6808ae7938be0ba46cc972491cad576d
ConorFogarty <46893180+ConorFogarty@users.noreply.github.com>
2021-05-05 20:46:57 +0100

    Next commit

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import tkinter as tk
+import logging
 
-def main():
-    print('Gay')
 
-if __name__ == '__main__':
-    main()
+logger = logging.getLogger()
+logger.debug("Important to debug")
+logger.info("Basic info")
+logger.warning("Warning message")
+logger.error("Helps to debug error")
+
+logger.setLevel(logging.INFO)
+
+steam_handler = logging.StreamHandler()
+formatter = logging.Formatter('')
+
+root = tk.Tk()
+root.mainloop()
+
