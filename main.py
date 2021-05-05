9cb606ec3438e4fcaf8b268179dc62fdafdaa42d
ConorFogarty <46893180+ConorFogarty@users.noreply.github.com>
2021-05-05 20:56:58 +0100

    Updated logging for Binance futures

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import tkinter as tk
 import logging
 
-
 logger = logging.getLogger()
-logger.debug("Important to debug")
-logger.info("Basic info")
-logger.warning("Warning message")
-logger.error("Helps to debug error")
 
 logger.setLevel(logging.INFO)
 
-steam_handler = logging.StreamHandler()
-formatter = logging.Formatter('')
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s :: %(message)s')
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
 
-root = tk.Tk()
-root.mainloop()
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.mainloop()
 
